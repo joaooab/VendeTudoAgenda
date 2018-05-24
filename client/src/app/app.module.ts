13a489a4f87e7a6e849471be2e4ee68571d095d9
Lucas <lucas_mr92@hotmail.com>
2018-05-24 12:30:43 -0300

    Implementando cadastro e listagem de usuário

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { RouterModule, Routes } from '@angular/router';
+import {ClienteRoutes} from './app.routes';
 
 import { AppComponent } from './app.component';
-
-
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { CadastroComponent } from './base/usuario/cadastro/cadastro.component';
+import { ListagemComponent } from './base/usuario/listagem/listagem.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    BreadcrumbComponent
+    BreadcrumbComponent,
+    CadastroComponent,
+    ListagemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    ClienteRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,18 +1,17 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginGuard } from './login/login.guard';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { ModuleWithProviders } from '@angular/compiler/src/core';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
-        canActivate: [LoginGuard],
+        //canActivate: [LoginGuard],
         path: '', children: [
-            {path: '', component: DashboardComponent},
-        
+            {path: '', component: DashboardComponent}
         ]
-    },
-
-
-]
+    }
+];
 
 export const ClienteRoutes: ModuleWithProviders = RouterModule.forRoot(routes, {useHash:true});

9690420d606a153bc81b56372febe3ae6eac20f2
Neilto Junior <shaka.leao@hotmail.com>
2018-05-28 07:51:34 -0300

    Request para resetar senha do usuário

--- server/src/main/java/VendeTudoAgenda/api/controller/AuthController.java
+++ server/src/main/java/VendeTudoAgenda/api/controller/AuthController.java
@@ -3,6 +3,7 @@ package VendeTudoAgenda.api.controller;
 import VendeTudoAgenda.api.exception.InvalidRequestException;
 import VendeTudoAgenda.core.repository.UsuarioRepository;
 import VendeTudoAgenda.core.services.JwtService;
+import VendeTudoAgenda.core.services.UsuarioService;
 import VendeTudoAgenda.domain.usuario.Usuario;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AuthController {
@@ -44,6 +43,23 @@ public class AuthController {
             throw new InvalidRequestException(bindingResult);
 
         }
+    }
+
+    @GetMapping("/resetarSenha/{id}")
+    public ResponseEntity resetarSenha(@PathVariable Long id){
+        Usuario usuario = usuarioRepository.findOne(id);
+
+        if(usuario != null){
+            String senhaNova = UsuarioService.gerarSenhaAleatoria();
+
+            usuario.setSenha(senhaNova);
+            usuarioRepository.save(usuario);
+
+            return new ResponseEntity(HttpStatus.OK);
+
+        }
+
+        return new ResponseEntity(HttpStatus.FAILED_DEPENDENCY);
     }
 
     private boolean checarSenha(String senhaBanco, String senhaRequest) {

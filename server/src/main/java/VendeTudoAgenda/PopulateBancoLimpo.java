b3b2612b4c69a30de779a55b26d6fc2384232c71
Neilto Junior <shaka.leao@hotmail.com>
2018-05-29 18:30:21 -0300

    Ajustando erro no PopulateBancoLimpo e no application.properties

--- server/src/main/java/VendeTudoAgenda/PopulateBancoLimpo.java
+++ server/src/main/java/VendeTudoAgenda/PopulateBancoLimpo.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-import sun.security.util.Password;
 
 import java.sql.Time;
 import java.util.Date;
@@ -28,15 +27,22 @@ public class PopulateBancoLimpo implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Usuario usuario = new Usuario("admin", 11111111111L, "vendetudoagenda@gmail.com", new Date(), "rua10", 38445654L, 985763251L, FuncaoUsuario.ADMINISTRADOR, "1234");
+        Usuario usuario = new Usuario("admin", 11111111111L, "vendetudoagenda@gmail.com", new Date(), "rua10", "38445654", "985763251", FuncaoUsuario.ADMINISTRADOR, "1234");
         incluirUsuario(usuario);
+
+        incluirUsuario(new Usuario("admin1", 22222222222L, "vendetudoagend@gmail.com", new Date(), "rua10", "38445654", "985763251", FuncaoUsuario.ADMINISTRADOR, "12345"));
+        incluirUsuario(new Usuario("admin2", 333L, "vendetudoagen@gmail.com", new Date(), "rua10", "38445654", "985763251", FuncaoUsuario.ADMINISTRADOR, "54321"));
+        incluirUsuario(new Usuario("admin3", 44444L, "vendetudoage@gmail.com", new Date(), "rua10", "38445654", "985763251", FuncaoUsuario.ADMINISTRADOR, "1234"));
+        incluirUsuario(new Usuario("admin4", 55555L, "vendetudoag@gmail.com", new Date(), "rua10", "38445654", "985763251", FuncaoUsuario.ADMINISTRADOR, "555857"));
+        incluirUsuario(new Usuario("admin5", 66666L, "vendetudoa@gmail.com", new Date(), "rua10", "38445654", "985763251", FuncaoUsuario.ADMINISTRADOR, "1234"));
+        incluirUsuario(new Usuario("admin6", 777777L, "vendetudo@gmail.com", new Date(), "rua10", "38445654", "985763251", FuncaoUsuario.ADMINISTRADOR, "1234"));
 
         Categoria categoria = new Categoria("PESSOA JURIDICA");
         categoria = incluirCategoria(categoria);
         categoria = new Categoria("PESSOA FISICA");
         categoria = incluirCategoria(categoria);
 
-        Contato contato = new Contato(categoria, usuario, "Joao", 11111111111L, null, "joao@email", new Date(), "rua12", 123132132L, 3522625L, true);
+        Contato contato = new Contato(categoria, usuario, "Joao", 11111111111L, null, "joao@email", new Date(), "rua12", "123132132", "3522625", true);
         contato = incluirContato(contato);
 
         Chamada chamada = new Chamada(contato, "Chamada - 1", new Date(), new Time(System.currentTimeMillis()), "Descricao da Chamada");

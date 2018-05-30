8ee6835ba2031b39e23d91e877545546a65c6b48
Lucas <lucas_mr92@hotmail.com>
2018-05-30 10:54:17 -0300

    Feito merge

--- client/src/app/arquitetura/servico/requisicao.ts
+++ client/src/app/arquitetura/servico/requisicao.ts
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
+
 import { Entidade } from "../../arquitetura/modelo/entidade.model";
-=======
-import { Entidade } from "../modelo/entidade.model";
->>>>>>> f68e512438b15d28c1ff4ecf1f03ecf976dc5dc7
+
+
 
 // export class Embedded<T extends Entidade>{
 //     t:T[];

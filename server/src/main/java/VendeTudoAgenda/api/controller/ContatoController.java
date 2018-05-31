fe8445686d0f02912d1670e8210c38998f7cdaec
Neilto Junior <shaka.leao@hotmail.com>
2018-05-31 19:03:22 -0300

    Ajustes na exportação

--- server/src/main/java/VendeTudoAgenda/api/controller/ContatoController.java
+++ server/src/main/java/VendeTudoAgenda/api/controller/ContatoController.java
@@ -15,13 +15,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class ContatoController {
-    
+
     private ContatoRepository contatoRepository;
     private ChamadaRepository chamadaRepository;
 
@@ -32,27 +34,27 @@ public class ContatoController {
     }
 
     @GetMapping("/contatoes/nome/{nome}")
-    public ResponseEntity buscarContatosNome(@PathVariable String nome){
+    public ResponseEntity buscarContatosNome(@PathVariable String nome) {
         Contato contato = contatoRepository.findByName(nome);
-        return contato != null ? ResponseEntity.ok(contato):ResponseEntity.notFound().build();
+        return contato != null ? ResponseEntity.ok(contato) : ResponseEntity.notFound().build();
     }
 
     @GetMapping("/contatoes/cpf/{cpf}")
-    public ResponseEntity buscarContatosCpf(@PathVariable Long cpf ) {
+    public ResponseEntity buscarContatosCpf(@PathVariable Long cpf) {
         Contato contato = contatoRepository.findByCpf(cpf);
-        return contato != null ? ResponseEntity.ok(contato):ResponseEntity.notFound().build();
+        return contato != null ? ResponseEntity.ok(contato) : ResponseEntity.notFound().build();
     }
 
     @GetMapping("/contatoes/cnpj/{cnpj}")
     public ResponseEntity buscarContatosCnpj(@PathVariable Long cnpj) {
         Contato contato = contatoRepository.findByCnpj(cnpj);
-        return contato != null ? ResponseEntity.ok(contato):ResponseEntity.notFound().build();
+        return contato != null ? ResponseEntity.ok(contato) : ResponseEntity.notFound().build();
     }
 
     @GetMapping("/contatoes/email/{email}")
     public ResponseEntity buscarContatosEmail(@PathVariable String email) {
         Contato contato = contatoRepository.findByEmail(email);
-        return contato != null ? ResponseEntity.ok(contato):ResponseEntity.notFound().build();
+        return contato != null ? ResponseEntity.ok(contato) : ResponseEntity.notFound().build();
     }
 
 
@@ -122,48 +124,61 @@ public class ContatoController {
 
     @GetMapping(value = "/gerarRelatorio", produces = "application/xls")
     public ResponseEntity getExcel() {
-        List<Contato> contatos = contatoRepository.findAll();
-        List<ContatoExcel> contatosExcel = new ArrayList<ContatoExcel>();
-        for (Contato contato : contatos) {
-            contatosExcel.add(new ContatoExcel(contato.getNome(), contato.getCategoria().getNome(), chamadaRepository.quantidadeLigacoes(contato.getId())));
-        }
-
-        HSSFWorkbook workbook = new HSSFWorkbook();
-        HSSFSheet sheet = workbook.createSheet("Relatório de Contatos");
-
-        Row header = sheet.createRow(0);
-        header.createCell(0).setCellValue("Nome");
-        header.createCell(1).setCellValue("Categoria");
-        header.createCell(2).setCellValue("Quantidade de Ligações");
-
-        int rowCount = 1;
-
-        for (ContatoExcel contato : contatosExcel) {
-            Row userRow = sheet.createRow(rowCount++);
-            userRow.createCell(0).setCellValue(contato.getNome());
-            userRow.createCell(1).setCellValue(contato.getCategoria());
-            userRow.createCell(2).setCellValue(contato.getQuantidadeLigacoes());
-
-        }
-
         try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            workbook.write(bos);
-            byte[] bytes = bos.toByteArray();
+            List<Contato> contatos = contatoRepository.findAll();
+            List<ContatoExcel> contatosExcel = new ArrayList<ContatoExcel>();
+            for (Contato contato : contatos) {
+                contatosExcel.add(new ContatoExcel(contato.getNome(), contato.getCategoria().getNome(), chamadaRepository.quantidadeLigacoes(contato.getId())));
+            }
 
-            byte[] bytes64 = Base64.encodeBase64(bytes);
-            String base64 = new String(bytes64);
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet sheet = workbook.createSheet("Relatório de Contatos");
+
+            Row header = sheet.createRow(0);
+            header.createCell(0).setCellValue("Nome");
+            header.createCell(1).setCellValue("Categoria");
+            header.createCell(2).setCellValue("Quantidade de Ligações");
+
+            int rowCount = 1;
+
+            for (ContatoExcel contato : contatosExcel) {
+                Row userRow = sheet.createRow(rowCount++);
+                userRow.createCell(0).setCellValue(contato.getNome());
+                userRow.createCell(1).setCellValue(contato.getCategoria());
+                userRow.createCell(2).setCellValue(contato.getQuantidadeLigacoes());
+
+            }
 
             HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"contatos.xls\"");
+            File desktopDir = new File(System.getProperty("user.home"), "Desktop");
 
-            return (new ResponseEntity(base64, headers, HttpStatus.OK));
+            if (desktopDir.exists()) {
+                String pathToDesktop = desktopDir.getPath();
+                FileOutputStream fileOut = new FileOutputStream(pathToDesktop + "\\contatos.xls");
+                workbook.write(fileOut);
+                fileOut.close();
+
+                return (new ResponseEntity("", headers, HttpStatus.OK));
+
+            } else {
+
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                workbook.write(bos);
+                byte[] bytes = bos.toByteArray();
+
+                byte[] bytes64 = Base64.encodeBase64(bytes);
+                String base64 = new String(bytes64);
+
+                headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"contatos.xls\"");
+
+                return (new ResponseEntity(base64, headers, HttpStatus.OK));
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return ResponseEntity.ok("erro");
+        return ResponseEntity.badRequest().build();
 
     }
 
